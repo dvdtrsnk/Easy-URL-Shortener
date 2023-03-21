df9f8a70eed023d10f296c70333a2985c80c6743
dvdtrsnk <122625462+dvdtrsnk@users.noreply.github.com>
2023-03-21 16:10:28 +0100

    Initial Commit

---
+++ Easy URL Shortener/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Easy URL Shortener
+//
+//  Created by David Třešňák on 21.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
