86e947c18397eff60be58a782c33dcda675e582e
dvdtrsnk <122625462+dvdtrsnk@users.noreply.github.com>
2023-03-22 16:10:53 +0100

    Nicely working textfield

---
+++ Easy URL Shortener/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Easy URL Shortener
+//
+//  Created by David Třešňák on 22.03.2023.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+struct K {
+    
+}
--- Easy URL Shortener/Control/ShortURLViewController.swift
+++ Easy URL Shortener/Control/ShortURLViewController.swift
@@ -10,47 +10,109 @@ import UIKit
 class ShortURLViewController: UIViewController {
 
     @IBOutlet weak var resultStackView: UIStackView!
-    
     @IBOutlet weak var noResultView: UIView!
-    
     @IBOutlet weak var problemResultView: UIView!
-    
     @IBOutlet weak var okResultView: UIView!
-    
     @IBOutlet weak var historyView: UIView!
+    @IBOutlet weak var bottomView: UIView!
+    @IBOutlet weak var bottomUrlView: UIView!
+    @IBOutlet weak var bottomBlurView: UIVisualEffectView!
+    @IBOutlet weak var bottomUrlViewPasteButton: UIButton!
+    @IBOutlet weak var bottomUrlViewCancelButton: UIButton!
+    @IBOutlet weak var bottomUrlViewBotConstraint: NSLayoutConstraint!
+    @IBOutlet weak var bottomUrlTextField: UITextField!
     
-    @IBOutlet weak var urlFieldView: UIView!
-    
-    @IBOutlet weak var bottomViewBotConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setUI()
+        registerNotifications()
+        updateUI()
+    }
+    
+    
+    //MARK: - Functions()
+    
+    private func setUI() {
         resultStackView.layer.cornerRadius = 10
         historyView.layer.cornerRadius = 10
-        urlFieldView.layer.cornerRadius = 10
+        bottomUrlView.layer.cornerRadius = 10
         noResultView.isHidden = false
         problemResultView.isHidden = false
         okResultView.isHidden = false
+    }
+    
+    private func updateUI() {
+        if bottomUrlTextField.text == nil || bottomUrlTextField.text == "" {
+            bottomUrlViewCancelButton.isHidden = true
+            bottomUrlViewPasteButton.isHidden = false
+        } else {
+            bottomUrlViewCancelButton.isHidden = false
+            bottomUrlViewPasteButton.isHidden = true
+        }
+    }
+    
+    private func registerNotifications() {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 
     @objc func keyboardWillShow(notification: NSNotification) {
         if let keyboardHeight = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height {
-            bottomViewBotConstraint.constant = 0 + keyboardHeight
+            bottomUrlViewBotConstraint.constant = 0 + keyboardHeight
             UIView.animate(withDuration: 0.3) {
                 self.view.layoutIfNeeded()
+                self.bottomView.backgroundColor = UIColor(named: "keyboardBackgroundColor")
+                self.bottomBlurView.isHidden = true
             }
         }
     }
     
     @objc func keyboardWillHide(notification: NSNotification) {
-        bottomViewBotConstraint.constant = 83
+        bottomUrlViewBotConstraint.constant = 83
         UIView.animate(withDuration: 0.3) {
             self.view.layoutIfNeeded()
+            self.bottomView.backgroundColor = .clear
+            self.bottomBlurView.isHidden = false
         }
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+
+    //MARK: - IBAction Buttons
+    
+    @IBAction func bottomUrlViewCancelButtonPressed(_ sender: Any) {
+        bottomUrlTextField.text = ""
+        updateUI()
+        bottomUrlTextField.becomeFirstResponder()
+    }
+    
+    @IBAction func bottomUrlViewPasteButtonPressed(_ sender: Any) {
+        if let clipboardString = UIPasteboard.general.string {
+            bottomUrlTextField.text = clipboardString
+            updateUI()
+        }
+    }
+    
+    
+    
 }
+
+//MARK: - UITextField Delegate
+extension ShortURLViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        updateUI()
+    }
+    
+
+}
+
 
