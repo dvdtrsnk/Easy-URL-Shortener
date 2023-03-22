c60892441451bc66311b85d7e856dee30ad45cd6
dvdtrsnk <122625462+dvdtrsnk@users.noreply.github.com>
2023-03-22 14:06:33 +0100

    Layout 3.0

--- Easy URL Shortener/Control/ShortURLViewController.swift
+++ Easy URL Shortener/Control/ShortURLViewController.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class ShortURLViewController: UIViewController {
 
+    @IBOutlet weak var resultStackView: UIStackView!
+    
+    @IBOutlet weak var noResultView: UIView!
+    
+    @IBOutlet weak var problemResultView: UIView!
+    
+    @IBOutlet weak var okResultView: UIView!
+    
+    @IBOutlet weak var historyView: UIView!
+    
+    @IBOutlet weak var urlFieldView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        resultStackView.layer.cornerRadius = 10
+        historyView.layer.cornerRadius = 10
+        urlFieldView.layer.cornerRadius = 10
+        noResultView.isHidden = false
+        problemResultView.isHidden = false
+        okResultView.isHidden = false
     }
 
 
