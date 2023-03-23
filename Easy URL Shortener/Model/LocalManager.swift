d126b49407ab7aad9ec249b98535044a98b3e817
dvdtrsnk <122625462+dvdtrsnk@users.noreply.github.com>
2023-03-23 18:08:01 +0100

    CoreData 2nd level

--- Easy URL Shortener/Model/LocalManager.swift
+++ Easy URL Shortener/Model/LocalManager.swift
@@ -7,9 +7,26 @@
 
 import Foundation
 import UIKit
+import CoreData
 
 struct LocalManager {
     var items = [SearchedItem]()
     
     
+    private func saveData() {
+        do {
+            try K.context.save()
+        } catch {
+            print("Error while saving data: \(error)")
+        }
+    }
+    
+    private mutating func loadData() {
+        let request : NSFetchRequest<SearchedItem> = SearchedItem.fetchRequest()
+        do {
+            self.items = try K.context.fetch(request)
+        } catch {
+            print("Error fetching data: \(error)")
+        }
+    }
 }
